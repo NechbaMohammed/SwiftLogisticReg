52a8a3bf16d255c3447c1e4691b82daea80d950f
unknown <mohammednechba@gmail.com>
2023-07-31 16:53:51 +0100

    medify logistic_cpu file

--- lib/logistic_cpu.py
+++ lib/logistic_cpu.py
@@ -1,60 +1,45 @@
 import numpy as np
 
-def sigmoid(x):
-    """
-    description: activation function (sigmoid)
-    args:
-        x: scalar
-    return: sigmoid of x
-    """
-    return 1 / (1 + np.exp(-x))
+class LogisticRegression:
+    def __init__(self, lr=0.2, epsilon=2e-2):
+        self.lr = lr
+        self.epsilon = epsilon
+        self.w = None
 
-def hs(x,w):
-    """
-    description: logistic regression hypothesis
-    args:
-        x: vector of features
-        w: vector of weights
-    return: matrcial product of x and w.T
-    """
-    return np.dot(w,x.T)
+    @staticmethod
+    def sigmoid(x):
+        return 1 / (1 + np.exp(-x))
 
-def LogisticRegression(X,Y, lr = 7, epsilon = 1e-4):
+    @staticmethod
+    def hs(x, w):
+        return np.dot(w, x.T)
 
-    # add bais to x
-    X = add_bias(X)
+    def fit(self, X, Y):
+        # add bias to x
+        X = self.add_bias(X)
+        self.w = np.zeros(X.shape[1]).reshape(1, X.shape[1])  # initialize weights vector
 
-    w = np.zeros(X.shape[1]).reshape(1,X.shape[1]) # initialize weights vector
+        while True:
+            gradient = ((self.sigmoid(self.hs(X, self.w)) - Y) @ X) * (1 / X.shape[0])
+            self.w -= self.lr * gradient  # gradient descent
+            
+            if np.linalg.norm(gradient) <= self.epsilon:
+                return self.w
 
-    while True:
+    def predict(self, X):
+        # add bias to x
+        X = self.add_bias(X)
+        # Calculate the dot product of X and the weight vector w
+        z = np.dot(X, self.w.T)
 
-        gradient = (( sigmoid(hs(X,w))-Y)  @  X)  *  (1/X.shape[0])
+        # Calculate the sigmoid of the dot product
+        predictions = 1 / (1 + np.exp(-z))
 
-        w -= lr* gradient # gradient descent
-        
-        if np.linalg.norm(gradient) <=epsilon:
-                    return w
+        # Round the predictions to the nearest integer (0 or 1)
+        predictions = np.round(predictions)
 
-def predict(X, w):
-    """
-    Predicts the labels for a given set of data using the trained logistic regression weights.
+        return predictions
 
-    Args:
-    X: numpy array, shape (m, n), input data
-    w: numpy array, shape (1, n), learned logistic regression weights
-
-    Returns:
-    predictions: numpy array, shape (m,), predicted labels
-    """
-    # add bais to x
-    X = add_bias(X)
-    # Calculate the dot product of X and the weight vector w
-    z = np.dot(X, w.T)
-
-    # Calculate the sigmoid of the dot product
-    predictions = 1 / (1 + np.exp(-z))
-
-    # Round the predictions to the nearest integer (0 or 1)
-    predictions = np.round(predictions)
-
-    return predictions
+    @staticmethod
+    def add_bias(X):
+        return np.hstack((np.ones((X.shape[0], 1)), X))
