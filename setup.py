c7bbe55aa6d9d30084c0223dbe66211c84b47b13
unknown <mohammednechba@gmail.com>
2023-08-03 15:12:13 +0100

    rename this package

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 from setuptools import setup
 from pathlib import Path
 
-# Read the contents of your README file
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+
 
 setup(
-    name="FastLogisticReg",
+    name="SwiftLogisticReg",
     version="0.0.1",
     author="Nechba Mohammed, Mouhajir Mohamed, and Sedjari Yassine",
     description="Efficient logistic regression in Python with GPU support",
-    long_description=long_description,
     long_description_content_type="text/markdown",  # Corrected the content type to "text/markdown"
-    url="https://github.com/NechbaMohammed/FastLogisticReg",
+    url="https://github.com/NechbaMohammed/SwiftLogisticReg",
     author_email="mohammednechba@gmail.com",
-    packages=['FastLogisticReg'],
+    packages=['SwiftLogisticReg'],
 )
