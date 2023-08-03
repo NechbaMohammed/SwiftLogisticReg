27770192be450c2fdc0f3526b40db05157f4b3d0
unknown <mohammednechba@gmail.com>
2023-08-03 14:45:07 +0100

    rename this package

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
-# read the contents of your README file
 from pathlib import Path
 
-# read the contents of your README file
+# Read the contents of your README file
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name = "fastlogreg",
-   version = "0.0.1",
-     author = "Nechba Mohammed, Mouhajir Mohamed, and Sedjari Yassine",
-description = "Efficient logistic regression in Python with GPU support",
-long_description =  long_description,
-long_description_content_type = "text/markdown",
-url = "https://github.com/NechbaMohammed/fastlogreg",
-author_email = "mohammednechba@gmail.com",
-       packages=['fastlogreg'],
-        zip_safe = False)
+setup(
+    name="FastLogisticReg",
+    version="0.0.1",
+    author="Nechba Mohammed, Mouhajir Mohamed, and Sedjari Yassine",
+    description="Efficient logistic regression in Python with GPU support",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Corrected the content type to "text/markdown"
+    url="https://github.com/NechbaMohammed/FastLogisticReg",
+    author_email="mohammednechba@gmail.com",
+    packages=['FastLogisticReg'],
+)
