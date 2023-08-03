ac77558d8c5eabde7854d79e52aab7d6f8749433
unknown <mohammednechba@gmail.com>
2023-08-03 13:08:45 +0100

    rewrite readme file

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from setuptools import setup
 # read the contents of your README file
 from pathlib import Path
 
+# read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(name = "fastlogistic",
-   version = "0.2",
+   version = "0.0.1",
      author = "Nechba Mohammed, Mouhajir Mohamed, and Sedjari Yassine",
 description = "Efficient logistic regression in Python with GPU support",
-long_description =  "file: README.md",
+long_description =  long_description,
 long_description_content_type = "text/markdown",
 url = "https://github.com/NechbaMohammed/fastlogistic",
 author_email = "mohammednechba@gmail.com",
